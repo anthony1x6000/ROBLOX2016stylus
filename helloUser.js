e6c97d136fdeffbc4309c255e3de282f25707cfd
Anthony <33004321+anthony1x6000@users.noreply.github.com>
2022-06-12 15:18:07 -0400

    made the HTML and CSS code  pretty

--- helloUser.js
+++ helloUser.js
@@ -21,40 +21,41 @@
   newDiv.setAttribute("id", "newdiv");
   const NA = "https://raw.githubusercontent.com/anthony1x6000/ROBLOX2016stylus/main/images/error.svg";
   const profileAVClass = `
-  height: 128px;
-  width: 128px;
-  margin-right: 15px;
-  margin-bottom: 0px;
+    height: 128px;
+    width: 128px;
+    margin-right: 15px;
+    margin-bottom: 0px;
   `;
   const displayNameClass = `
-  font-size: 30px;
-  margin-top: calc(128px / 3);
+    font-size: 30px;
+    margin-top: calc(128px / 3);
   `;
   homeID.parentNode.insertBefore(newDiv, homeID);
   newDiv.innerHTML = `
   <a class="dynamic-overflow-container text-nav" href="https://www.roblox.com/users/${userId}/profile" role="link">
-<span id="profileAV" class="avatar avatar-headshot-xs" style="${profileAVClass}">
-  <span class="thumbnail-2d-container avatar-card-image">
-    <img class="" src="${NA}" alt="Loading..." title="Loading...">
-  </span>
-</span>
-<div id="displayName" class="font-header-2 dynamic-ellipsis-item" style="${displayNameClass}">Hello, ${userDisplayName}!</div>
-</a>`;
+    <span id="profileAV" class="avatar avatar-headshot-xs" style="${profileAVClass}">
+      <span class="thumbnail-2d-container avatar-card-image">
+        <img class="" src="${NA}" alt="Loading..." title="Loading...">
+      </span>
+    </span>
+    <div id="displayName" class="font-header-2 dynamic-ellipsis-item" style="${displayNameClass}">Hello, ${userDisplayName}!</div>
+  </a>
+  `;
 
-  window.addEventListener('load', function() { // WAIT FOR PAGE LOAD. listening to dprk music rn
+  window.addEventListener('load', function() { // WAIT FOR PAGE LOAD. listening to dprk music rn. becoming juchepilled by the second!!!! 
       const profileLink = `https://www.roblox.com/users/${userId}/profile`;
       const profileAV = document.querySelector("#navigation > ul > li:nth-child(1) > a > span > span > img").src;
       const avT = document.querySelector("#navigation > ul > li:nth-child(1)").innerHTML;
       console.log("TEST LOG!!!", profileAV);
       newDiv.innerHTML = `
-  <a class="dynamic-overflow-container text-nav" href="https://www.roblox.com/users/${userId}/profile" role="link">
-<span id="profileAV" class="avatar avatar-headshot-xs" style="${profileAVClass}">
-  <span class="thumbnail-2d-container avatar-card-image">
-    <img class="" src="${profileAV}" alt="${userName}" title="${userName}">
-  </span>
-</span>
-<div id="displayName" class="font-header-2 dynamic-ellipsis-item" style="${displayNameClass}">Hello, ${userDisplayName}!</div>
-</a>
-  `;
+      <a class="dynamic-overflow-container text-nav" href="https://www.roblox.com/users/${userId}/profile" role="link">
+        <span id="profileAV" class="avatar avatar-headshot-xs" style="${profileAVClass}">
+          <span class="thumbnail-2d-container avatar-card-image">
+            <img class="" src="${profileAV}" alt="${userName}" title="${userName}">
+          </span>
+        </span>
+        <div id="displayName" class="font-header-2 dynamic-ellipsis-item" style="${displayNameClass}">Hello, ${userDisplayName}!</div>
+      </a>
+      `;
 }, false);
 })();
