7668a353a96d0a5644bf8c2c994203111d20ce92
anthop <33004321+anthony1x6000@users.noreply.github.com>
2022-09-12 22:09:15 -0400

    Add files via upload
    Changes title and icon to be like 2016

---
+++ favicon.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Favicon and Title
+// @namespace    https://github.com/anthony1x6000/ROBLOX2016stylus
+// @version      0.1
+// @description  Change roblox icon and title
+// @author       anthony1x6000
+// @match        https://www.roblox.com/home
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=roblox.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var icon = document.querySelector("link[rel~='icon']");
+    icon.href = 'https://images.rbxcdn.com/7aee41db80c1071f60377c3575a0ed87.ico';
+
+    var title = document.querySelector("head > title").innerHTML;
+    title = title.replace('Roblox', 'ROBLOX');
+    document.querySelector("head > title").innerHTML = title;
+})();
