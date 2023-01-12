0fd86cb88681e1a0301426cd7b3f97ba7b283fbe
anthop <33004321+anthony1x6000@users.noreply.github.com>
2023-01-12 16:59:13 -0500

    only observe right-navigation-header
    for the search

--- legacywords.user.js
+++ legacywords.user.js
@@ -2,10 +2,10 @@
 // @name         Word change
 // @namespace    https://github.com/anthony1x6000/ROBLOX2016stylus
 // @version      0.1
-// @description  replace words
+// @description  try to take over the world!
 // @author       anthony1x6000
 // @match        https://www.roblox.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=roblox.com
+// @icon         http://images.rbxcdn.com/7aee41db80c1071f60377c3575a0ed87.ico
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
@@ -30,15 +30,23 @@
     }
 
     function runafter() { //replace your words here (word to replace, replacement)
-        // console.log('Running replacement');
+        console.log('Running replacement');
         replaceWord('Avatar Shop', 'Catalog');
         replaceWord('Creator Marketplace', 'Library');
         replaceWord('Experiences', 'Games');
     }
     window.addEventListener('load', runafter, false);
+
+    const elementIds = ["right-navigation-header"];
     const observer = new MutationObserver(runafter);
-    observer.observe(document, {
-        childList: true,
-        subtree: true
-    });
+    for (const id of elementIds) {
+        const element = document.getElementById(id);
+        if (element) {
+            observer.observe(element, {
+                childList: true,
+                subtree: true
+            });
+        }
+    }
+    // observer.disconnect(); // uncomment if performance is hit
 })();
